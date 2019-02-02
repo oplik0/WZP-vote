815bbe88dcce5273c8341c2f2c3f023df7f99590
opliko95 <opliko95@users.noreply.github.com>
2019-02-02 17:32:36 +0100

    Poprawki i usunięcie jednak niepotrzebnych bibliotek

--- wzp.py
+++ wzp.py
@@ -2,9 +2,10 @@ from flask import Flask, jsonify, render_template, request
 app = Flask(__name__)
 @app.route("/save", methods=["post"])
 def save():
-    user = str(request.values.get('user'))
-    text = str(request.values.get('text'))
-    password = str(request.values.get('password'))
+    print("working")
+    user = str(request.json['user'])
+    text = str(request.json['text'])
+    password = str(request.json['password'])
     try:
         with open("writable.txt", 'r') as writable:
             wr = int(writable.read())
@@ -18,11 +19,11 @@ def save():
                 else:
                     return(jsonify({"error":"password"}))
         else:
-            with open(user+"vote.txt", "w") as saved_file:
+            with open(user+"_vote.txt", "r") as saved_file:
                 output = str(saved_file.read())
                 return(jsonify({"error":"none", "type":"decryption","text":output}))
     except Exception as E:
-        print(E)
+        print("Error:", E)
         return(jsonify({"error":"error"}))
 @app.route('/')
 def index():
@@ -31,4 +32,4 @@ def index():
 if __name__=="__main__":
     #context = ('cert.pem', 'key.pem')
     #app.run(host="0.0.0.0", port="443", ssl_context=context)
-    app.run(host="0.0.0.0", port=80)
+    app.run(host="127.0.0.1", port=5000)
