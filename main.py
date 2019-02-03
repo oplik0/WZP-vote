49bdd903032756a40e83b5c9e18363b0e6545fb6
opliko95 <opliko95@users.noreply.github.com>
2019-02-03 23:19:54 +0100

    Zmiana struktury plików głosowania - teraz głosy i hasła znajdują się w oddzielnych folderach

--- main.py
+++ main.py
@@ -10,16 +10,16 @@ def save():
         with open("writable.txt", 'r') as writable:
             wr = int(str(writable.read()).replace("\n", ""))
         if wr:
-            with open(user+"_password.txt", "r") as password_file:
+            with open("passwords/"+user+"_password.txt", "r") as password_file:
 
                 if password == str(password_file.read()).replace("\n", ""):
-                    with open(user+"_vote.txt", "w") as saved_file:
+                    with open("votes/"+user+"_vote.txt", "w") as saved_file:
                         saved_file.write(text)
                         return(jsonify({"error":"none", "type":"encryption"}))
                 else:
                     return(jsonify({"error":"password"}))
         else:
-            with open(user+"_vote.txt", "r") as saved_file:
+            with open("votes/"+user+"_vote.txt", "r") as saved_file:
                 output = str(saved_file.read())
                 return(jsonify({"error":"none", "type":"decryption","text":output}))
     except Exception as E:
